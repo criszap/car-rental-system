87dfb3e18fb70e9850253ea1e1389c3f1e28133d
criszap <czapata011101@gmail.com>
2022-11-22 21:33:30 -0600

    added madeby label (test)

--- DBProject/Form1.Designer.cs
+++ DBProject/Form1.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
@@ -41,11 +42,22 @@
             this.label1.TabIndex = 0;
             this.label1.Text = "Car Rental System";
             // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(12, 428);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(275, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Made By: Cristian Zapata && Alejandro Romero-Lopez";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -57,5 +69,6 @@
         #endregion
 
         private Label label1;
+        private Label label2;
     }
 }

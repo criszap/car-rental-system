d29c168288ea4ca38ab95eca99820298480d3c84
aromero38 <71117150+aromero38@users.noreply.github.com>
2022-11-23 16:42:36 -0600

    Setup basic layout, adding Car List template

---
+++ DBProject/CarRentalSystem.Designer.cs
@@ -0,0 +1,282 @@
+﻿namespace DBProject
+{
+    partial class CarRentalSystem
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label2 = new System.Windows.Forms.Label();
+            this.Make = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.Model = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.Year = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.Color = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.DailyPrice = new System.Windows.Forms.Label();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.carList = new System.Windows.Forms.TabPage();
+            this.customers = new System.Windows.Forms.TabPage();
+            this.rentedCars = new System.Windows.Forms.TabPage();
+            this.rentalInfo = new System.Windows.Forms.TabPage();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.tabControl1.SuspendLayout();
+            this.carList.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(12, 553);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(275, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Made By: Cristian Zapata && Alejandro Romero-Lopez";
+            // 
+            // Make
+            // 
+            this.Make.AutoSize = true;
+            this.Make.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.Make.Location = new System.Drawing.Point(92, 92);
+            this.Make.Name = "Make";
+            this.Make.Size = new System.Drawing.Size(47, 20);
+            this.Make.TabIndex = 2;
+            this.Make.Text = "Make";
+            this.Make.Click += new System.EventHandler(this.Make_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.AcceptsTab = true;
+            this.textBox1.BackColor = System.Drawing.SystemColors.Window;
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox1.Location = new System.Drawing.Point(92, 115);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(119, 23);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // Model
+            // 
+            this.Model.AutoSize = true;
+            this.Model.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.Model.Location = new System.Drawing.Point(244, 92);
+            this.Model.Name = "Model";
+            this.Model.Size = new System.Drawing.Size(53, 20);
+            this.Model.TabIndex = 4;
+            this.Model.Text = "Model";
+            this.Model.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.AcceptsTab = true;
+            this.textBox2.BackColor = System.Drawing.SystemColors.Window;
+            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox2.Location = new System.Drawing.Point(244, 115);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(119, 23);
+            this.textBox2.TabIndex = 5;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // Year
+            // 
+            this.Year.AutoSize = true;
+            this.Year.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.Year.Location = new System.Drawing.Point(399, 92);
+            this.Year.Name = "Year";
+            this.Year.Size = new System.Drawing.Size(39, 20);
+            this.Year.TabIndex = 6;
+            this.Year.Text = "Year";
+            this.Year.Click += new System.EventHandler(this.label1_Click_1);
+            // 
+            // textBox3
+            // 
+            this.textBox3.AcceptsTab = true;
+            this.textBox3.BackColor = System.Drawing.SystemColors.Window;
+            this.textBox3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox3.Location = new System.Drawing.Point(399, 115);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(119, 23);
+            this.textBox3.TabIndex = 7;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // textBox4
+            // 
+            this.textBox4.AcceptsTab = true;
+            this.textBox4.BackColor = System.Drawing.SystemColors.Window;
+            this.textBox4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox4.Location = new System.Drawing.Point(555, 115);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(119, 23);
+            this.textBox4.TabIndex = 9;
+            this.textBox4.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            // 
+            // Color
+            // 
+            this.Color.AutoSize = true;
+            this.Color.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.Color.Location = new System.Drawing.Point(555, 92);
+            this.Color.Name = "Color";
+            this.Color.Size = new System.Drawing.Size(46, 20);
+            this.Color.TabIndex = 8;
+            this.Color.Text = "Color";
+            this.Color.Click += new System.EventHandler(this.Color_Click);
+            // 
+            // textBox5
+            // 
+            this.textBox5.AcceptsTab = true;
+            this.textBox5.BackColor = System.Drawing.SystemColors.Window;
+            this.textBox5.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox5.Location = new System.Drawing.Point(702, 115);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(119, 23);
+            this.textBox5.TabIndex = 11;
+            this.textBox5.TextChanged += new System.EventHandler(this.textBox5_TextChanged);
+            // 
+            // DailyPrice
+            // 
+            this.DailyPrice.AutoSize = true;
+            this.DailyPrice.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.DailyPrice.Location = new System.Drawing.Point(702, 92);
+            this.DailyPrice.Name = "DailyPrice";
+            this.DailyPrice.Size = new System.Drawing.Size(82, 20);
+            this.DailyPrice.TabIndex = 10;
+            this.DailyPrice.Text = "Daily Price";
+            this.DailyPrice.Click += new System.EventHandler(this.DailyPrice_Click);
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.carList);
+            this.tabControl1.Controls.Add(this.customers);
+            this.tabControl1.Controls.Add(this.rentedCars);
+            this.tabControl1.Controls.Add(this.rentalInfo);
+            this.tabControl1.Location = new System.Drawing.Point(12, 12);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(961, 538);
+            this.tabControl1.TabIndex = 12;
+            // 
+            // carList
+            // 
+            this.carList.Controls.Add(this.dataGridView1);
+            this.carList.Controls.Add(this.textBox5);
+            this.carList.Controls.Add(this.Make);
+            this.carList.Controls.Add(this.DailyPrice);
+            this.carList.Controls.Add(this.textBox1);
+            this.carList.Controls.Add(this.textBox4);
+            this.carList.Controls.Add(this.Model);
+            this.carList.Controls.Add(this.Color);
+            this.carList.Controls.Add(this.textBox2);
+            this.carList.Controls.Add(this.textBox3);
+            this.carList.Controls.Add(this.Year);
+            this.carList.Location = new System.Drawing.Point(4, 24);
+            this.carList.Name = "carList";
+            this.carList.Padding = new System.Windows.Forms.Padding(3);
+            this.carList.Size = new System.Drawing.Size(953, 510);
+            this.carList.TabIndex = 0;
+            this.carList.Text = "Car List";
+            this.carList.UseVisualStyleBackColor = true;
+            // 
+            // customers
+            // 
+            this.customers.Location = new System.Drawing.Point(4, 24);
+            this.customers.Name = "customers";
+            this.customers.Padding = new System.Windows.Forms.Padding(3);
+            this.customers.Size = new System.Drawing.Size(953, 510);
+            this.customers.TabIndex = 1;
+            this.customers.Text = "Customers";
+            this.customers.UseVisualStyleBackColor = true;
+            // 
+            // rentedCars
+            // 
+            this.rentedCars.Location = new System.Drawing.Point(4, 24);
+            this.rentedCars.Name = "rentedCars";
+            this.rentedCars.Size = new System.Drawing.Size(953, 510);
+            this.rentedCars.TabIndex = 2;
+            this.rentedCars.Text = "Rented Cars";
+            this.rentedCars.UseVisualStyleBackColor = true;
+            // 
+            // rentalInfo
+            // 
+            this.rentalInfo.Location = new System.Drawing.Point(4, 24);
+            this.rentalInfo.Name = "rentalInfo";
+            this.rentalInfo.Size = new System.Drawing.Size(953, 510);
+            this.rentalInfo.TabIndex = 3;
+            this.rentalInfo.Text = "Rental Info";
+            this.rentalInfo.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(6, 168);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 25;
+            this.dataGridView1.Size = new System.Drawing.Size(941, 336);
+            this.dataGridView1.TabIndex = 12;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // CarRentalSystem
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(985, 575);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.label2);
+            this.Name = "CarRentalSystem";
+            this.Text = "Car Rental System";
+            this.Load += new System.EventHandler(this.CarList_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.carList.ResumeLayout(false);
+            this.carList.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private Label label2;
+        private Label Make;
+        private TextBox textBox1;
+        private Label Model;
+        private TextBox textBox2;
+        private Label Year;
+        private TextBox textBox3;
+        private TextBox textBox4;
+        private Label Color;
+        private TextBox textBox5;
+        private Label DailyPrice;
+        private TabControl tabControl1;
+        private TabPage carList;
+        private TabPage customers;
+        private TabPage rentedCars;
+        private TabPage rentalInfo;
+        private DataGridView dataGridView1;
+    }
+}
---
+++ DBProject/CarRentalSystem.cs
@@ -0,0 +1,70 @@
+namespace DBProject
+{
+    public partial class CarRentalSystem : Form
+    {
+        public CarRentalSystem()
+        {
+            InitializeComponent();
+        }
+
+        private void CarList_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Make_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Color_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void DailyPrice_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
